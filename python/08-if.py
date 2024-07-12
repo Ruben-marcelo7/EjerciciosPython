bb6d6aaf4ec5ee3cb1167c89348bd2b1c625d35b
Rubén <ruben.cobos@tecnologicoliceocristiano.edu.ec>
2024-07-11 20:30:48 -0500

    python

---
+++ python/08-if.py
@@ -0,0 +1,10 @@
+puntaje = 93
+
+if puntaje >= 95:
+    print ("Aprovado con honores")
+elif puntaje >= 50:
+    print("Alumno aprovado")
+else:
+    print("Alumno reprovado")
+
+print("Fuera del if")
